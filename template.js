211d5e7685ce45f9f4ade60a9c9be310f1138d38
KingNook <mike04.lin@gmail.com>
2019-12-28 20:12:03 +0000

    Update template.js

--- template.js
+++ template.js
@@ -3,7 +3,7 @@ var pages = ['index', 'chess'];
 var navbar = document.createElement('ul');
 
 pages.forEach(function(e) {
-    let navitem = document.createElement('li');
+    let navitem = document.createElement('li').appendChild(document.createElement('a'));
     navitem.setAttribute('href', '/'+e);
     navitem.innerHTML = e;
     navbar.appendChild(navitem);
