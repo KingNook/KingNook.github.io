aeb41a55de56c8b461af5fae6f0a755bb0fb63e2
KingNook <mike04.lin@gmail.com>
2019-12-28 20:09:49 +0000

    Update template.js

--- template.js
+++ template.js
@@ -4,7 +4,7 @@ var navbar = document.createElement('ul');
 
 pages.forEach(function(e) {
     let navitem = document.createElement('li');
-    navitem.href = '/'+e;
+    navitem.setAttribute('href', '/'+e);
     navitem.innerHTML = e;
     navbar.appendChild(navitem);
 })
