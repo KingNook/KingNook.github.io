41aa73fa013742e63219ecd193d382d71f6c137a
KingNook <mike04.lin@gmail.com>
2019-12-29 09:57:07 +0000

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,3 +1,11 @@
 import * as Classes from './classes.js'
 
-var pagelist = ['index', 'chess'];
+var pagenames = ['index', 'chess'];
+var pagelist = new Array;
+
+pagenames.forEach(function(e) {
+    let page = new Classes.Page(e, '/'+e);
+    pagelist.push(page);
+})
+
+console.log(pagelist)
