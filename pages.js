e23ec60c87e207da986d26c3ccaf7abbf9365f4f
KingNook <mike04.lin@gmail.com>
2019-12-29 10:15:25 +0000

    Update pages.js

--- pages.js
+++ pages.js
@@ -3,11 +3,6 @@ import * as Classes from './classes.js'
 var pagenames = ['index', 'chess'];
 export var pagelist = new Array(0);
 
-pagenames.forEach(function(e) {
-    let page = new Classes.Page(e, '/'+e);
-    pagelist.push(page);
-})
-
 // INDEX
 {
     let page = new Classes.Page('index', '/index');
@@ -26,3 +21,4 @@ pagenames.forEach(function(e) {
     pagelist.push(page);
 }
 
+console.log(pagelist);
